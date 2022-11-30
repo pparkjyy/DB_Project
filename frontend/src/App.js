0c9c2033b599dd6967af0de13248a9480722279e
JeonSeungGwon <jeon87721@gmail.com>
2022-11-30 21:54:34 +0900

    DB-Connect

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,10 @@
-
+import './App.css'
 
 function App() {
   return (
-    <>
-    
-    </>
+    <div>
+      <a className="linktoDB" href="http://localhost:3001/">DB 연동 확인</a>
+    </div>
   );
 }
 
