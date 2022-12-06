bd8f49fedaa58c84aa78032e4fbe9b8d2556631f
seyeon326 <89377951+seyeon326@users.noreply.github.com>
2022-12-07 04:24:51 +0900

    홈 화면 프론트

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -20,6 +20,43 @@ const Body = styled.div`
   justify-content: "center";
   width: 100%;
 `;
+const Ranking = styled.div`
+  width: 40%;
+  height: 500px;
+  padding-top: 100px;
+  padding-left: 64px;
+  float: left;
+`
+const RankTitle = styled.div`
+  align-items: center;
+`
+const Title = styled.div`
+  padding-top: 48px;
+  padding-bottom: 30px;
+  padding-left: 64px;
+  font-size: 35px;
+  font-weight: bold;
+`;
+const NewsWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  padding-left: 65px;
+  align-items: left;
+  justify-content: left;
+`;
+const NewsTitle = styled.div`
+  padding-bottom: 10px;
+  font-size: 18px;
+  float: left;
+`;
+const NewsOffice = styled.div`
+  padding-bottom: 10px;
+  font-size: 15px;
+  color: #808080;
+  float: left;
+  padding-left: 50px;
+  padding-top: 3px;
+`;
 const Tr = styled.tr`
   border-top: 1px solid black;
   border-bottom: 1px solid black;
@@ -64,7 +101,7 @@ const Home = ({ history }) => {
   return (
     <Body style={{}}>
       <CardWrapper>
-        <h1>메인 페이지</h1>
+        {/* <h1>메인 페이지</h1>
         <div>에서 DB 연결 테스트</div>
         <div>코드는 backend/src/api/test.js 랑</div>
         <div>backend/src/server.js 에서 확인 가능</div>
@@ -80,7 +117,79 @@ const Home = ({ history }) => {
             <Td>money</Td>
           </TitleTr>
           {printData(Data)}
-        </table>
+        </table> */}
+
+        <Title>오늘의 뉴스</Title>
+        <NewsWrapper>
+          <div>
+            <NewsTitle>[시황] 코스피, 외인 매도에 2260대 마감</NewsTitle><NewsOffice>데일리안</NewsOffice>
+          </div>
+          <div>
+            <NewsTitle>[시황] 코스피, 외인 매도에 2260대 마감</NewsTitle><NewsOffice>데일리안</NewsOffice>
+          </div>
+          <div>
+            <NewsTitle>[시황] 코스피, 외인 매도에 2260대 마감</NewsTitle><NewsOffice>데일리안</NewsOffice>
+          </div>
+          <div>
+            <NewsTitle>[시황] 코스피, 외인 매도에 2260대 마감</NewsTitle><NewsOffice>데일리안</NewsOffice>
+          </div>
+          <div>
+            <NewsTitle>[시황] 코스피, 외인 매도에 2260대 마감</NewsTitle><NewsOffice>데일리안</NewsOffice>
+          </div>
+        </NewsWrapper>
+
+        <Ranking>
+          <RankTitle>상승률 상위</RankTitle>
+            <div>1. 이수화학 25,200 5,800 +29.90%</div>
+            <div>1. 이수화학 25,200 5,800 +29.90%</div>
+            <div>1. 이수화학 25,200 5,800 +29.90%</div>
+            <div>1. 이수화학 25,200 5,800 +29.90%</div>
+            <div>1. 이수화학 25,200 5,800 +29.90%</div>
+            <div>1. 이수화학 25,200 5,800 +29.90%</div>
+            <div>1. 이수화학 25,200 5,800 +29.90%</div>
+            <div>1. 이수화학 25,200 5,800 +29.90%</div>
+            <div>1. 이수화학 25,200 5,800 +29.90%</div>
+            <div>1. 이수화학 25,200 5,800 +29.90%</div>
+        </Ranking>
+        <Ranking>
+          <RankTitle>하락률 상위</RankTitle>
+            <div>1. 이수화학 25,200 5,800 +29.90%</div>
+            <div>1. 이수화학 25,200 5,800 +29.90%</div>
+            <div>1. 이수화학 25,200 5,800 +29.90%</div>
+            <div>1. 이수화학 25,200 5,800 +29.90%</div>
+            <div>1. 이수화학 25,200 5,800 +29.90%</div>
+            <div>1. 이수화학 25,200 5,800 +29.90%</div>
+            <div>1. 이수화학 25,200 5,800 +29.90%</div>
+            <div>1. 이수화학 25,200 5,800 +29.90%</div>
+            <div>1. 이수화학 25,200 5,800 +29.90%</div>
+            <div>1. 이수화학 25,200 5,800 +29.90%</div>
+        </Ranking>
+        <Ranking>
+          <RankTitle>거래량</RankTitle>
+            <div>1. 이수화학 25,200 5,800 +29.90%</div>
+            <div>1. 이수화학 25,200 5,800 +29.90%</div>
+            <div>1. 이수화학 25,200 5,800 +29.90%</div>
+            <div>1. 이수화학 25,200 5,800 +29.90%</div>
+            <div>1. 이수화학 25,200 5,800 +29.90%</div>
+            <div>1. 이수화학 25,200 5,800 +29.90%</div>
+            <div>1. 이수화학 25,200 5,800 +29.90%</div>
+            <div>1. 이수화학 25,200 5,800 +29.90%</div>
+            <div>1. 이수화학 25,200 5,800 +29.90%</div>
+            <div>1. 이수화학 25,200 5,800 +29.90%</div>
+        </Ranking>
+        <Ranking>
+          <RankTitle>시가총액</RankTitle>
+            <div>1. 이수화학 25,200 5,800 +29.90%</div>
+            <div>1. 이수화학 25,200 5,800 +29.90%</div>
+            <div>1. 이수화학 25,200 5,800 +29.90%</div>
+            <div>1. 이수화학 25,200 5,800 +29.90%</div>
+            <div>1. 이수화학 25,200 5,800 +29.90%</div>
+            <div>1. 이수화학 25,200 5,800 +29.90%</div>
+            <div>1. 이수화학 25,200 5,800 +29.90%</div>
+            <div>1. 이수화학 25,200 5,800 +29.90%</div>
+            <div>1. 이수화학 25,200 5,800 +29.90%</div>
+            <div>1. 이수화학 25,200 5,800 +29.90%</div>
+        </Ranking>
       </CardWrapper>
     </Body>
   );
