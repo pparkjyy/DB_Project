68c0e6c14c455f3d7ccb275bb95b046d728b44f3
lllminsuk <umsun3@naver.com>
2022-12-11 04:03:16 +0900

    mypage->보유주식->전일대비 부호 반대로 나오는 오류 수정

--- backend/src/api/User/calculatestock.js
+++ backend/src/api/User/calculatestock.js
@@ -4,7 +4,7 @@ export default async (app, connection) => {
   app.use("/calculatestock", async (req, res, next) => {
     const { id } = req.query;
     await connection.query(
-      "SELECT code, stock_name 종목명, n_price 현재가격, sum(stock_num*stock_price)/sum(stock_num) 구매가격, (n_price-(sum(stock_num*stock_price)/sum(stock_num)))/(sum(stock_num*stock_price)/sum(stock_num))*100 평가손익, e_price-n_price 전일대비, (n_price-e_price)/e_price*100 전일대비비율 FROM STOCK_U natural join STOCK_P natural join STOCK WHERE ID=? and date='2022-12-08'group by code;",
+      "SELECT code, stock_name 종목명, n_price 현재가격, sum(stock_num*stock_price)/sum(stock_num) 구매가격, (n_price-(sum(stock_num*stock_price)/sum(stock_num)))/(sum(stock_num*stock_price)/sum(stock_num))*100 평가손익, n_price-e_price 전일대비, (n_price-e_price)/e_price*100 전일대비비율 FROM STOCK_U natural join STOCK_P natural join STOCK WHERE ID=? and date='2022-12-08'group by code;",
       [id],
       (error, data) => {
         if (error) console.log(error);
